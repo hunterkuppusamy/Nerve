1bdd0f29fc3b8b8dcb9b97b49e9c31f7c4b7a0b5
hunterkuppusamy <hunterkupp@gmail.com>
2024-10-08 17:15:39 -0400

    Trying to fix publication in the build script.
    Took 50 minutes

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,11 +7,13 @@ plugins {
     `maven-publish`
 }
 
-group = "dev.hunter.nerve"
+group = "dev.hunter"
 version = "0.0.1"
 
 kotlin {
-    jvm()
+    jvm {
+        withSourcesJar(publish = false)
+    }
     // no reason for these really...
     // linuxX64()
     // mingwX64()
@@ -62,48 +64,56 @@ compose.desktop {
 
         nativeDistributions {
             targetFormats(TargetFormat.Exe, TargetFormat.AppImage, TargetFormat.Rpm)
-            packageName = "dev.hunter.nerve"
+            packageName = "dev.hunter"
             packageVersion = "0.0.1"
             includeAllModules = true
         }
     }
 }
 
-tasks {
-    publish {
-        publishing {
-            repositories {
-                maven {
-                    // change to point to your repo, e.g. http://my.org/repo
-                    name = "github"
-                    url = uri("https://maven.pkg.github.com/hunterkuppusamy/Nerve")
-                    credentials {
-                        username = "hunterkuppusamy"
-                        password = System.getenv("TOKEN") ?: System.getenv("GITHUB_TOKEN") ?: project.findProperty("NERVE_PUBLISHING_TOKEN").toString()
-                    }
-                }
-            }
-            publications {
-                create<MavenPublication>("Nerve") {
-                    pom {
-                        name = "Nerve"
-                        description = "An interpreted language made on Tuesday"
-                        url = "https://github.com/hunterkuppusamy/Nerve"
-                        developers {
-                            developer {
-                                id = "hunter"
-                                name = "Hunter Kuppusamy"
-                                email = "hunterkupp@gmail.com"
-                            }
-                        }
-                    }
-                    groupId = project.group.toString()
-                    artifactId = project.name.lowercase()
-                    version = project.version.toString()
-
-                    from(getComponents()["kotlin"]) // the property delegate for the function is actually overloaded and idk how to not do that!
-                }
+publishing {
+    repositories {
+        maven {
+            // change to point to your repo, e.g. http://my.org/repo
+            name = "github"
+            url = uri("https://maven.pkg.github.com/hunterkuppusamy/nerve")
+            credentials {
+                username = "hunterkuppusamy"
+                password = System.getenv("TOKEN") ?: System.getenv("GITHUB_TOKEN") ?: project.findProperty("NERVE_PUBLISHING_TOKEN").toString()
             }
         }
+    }
+    publications {
+        create<MavenPublication>("maven") {
+            artifactId = project.name.lowercase()
+            groupId = project.group.toString().lowercase()
+            version = project.version.toString()
+
+            pom {
+                name = "Nerve"
+                description = "An interpreted language made on Tuesday"
+                url = "https://github.com/hunterkuppusamy/nerve"
+                developers {
+                    developer {
+                        id = "hunter"
+                        name = "Hunter Kuppusamy"
+                        email = "hunterkupp@gmail.com"
+                    }
+                }
+            }
+
+            println("artificat = $group:$artifactId:$version")
+
+            from(getComponents()["kotlin"]) // the property delegate for the function is actually overloaded and idk how to not do that!
+        }
+    }
+}
+
+tasks {
+    publish {
+
+    }
+    build {
+
     }
 }
